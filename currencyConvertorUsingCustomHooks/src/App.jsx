93c43fad5938dcdde9e119f86e699ae9ce62742f
VirajYan <virrrajyan@gmail.com>
2025-02-27 01:42:51 +0530

    rest

---
+++ currencyConvertorUsingCustomHooks/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import { InputBox } from './components'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
+import './App.css'
+
+function App() {
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from)
+
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
+
+  const convert = () =>{
+    setConvertedAmount(amount * currencyInfo[to])
+  }
+
+  const options = Object.keys(currencyInfo)
+  return (
+    <>
+    <h1 className='text-2xl bg-orange-500'>Currency Converter</h1>
+    </>
+  )
+}
+
+export default App
